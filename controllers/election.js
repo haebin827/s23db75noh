2bdb7dc4610c666eaff46d2a4d6fb8b523ead57d
haebin827 <99354243+haebin827@users.noreply.github.com>
2023-11-08 18:22:34 -0600

    Part 7

--- controllers/election.js
+++ controllers/election.js
@@ -3,24 +3,24 @@ var Election = require('../models/election');
 exports.election_list = function(req, res) {
 res.send('NOT IMPLEMENTED: Election list');
 };
-// for a specific Costume.
+// for a specific Election.
 exports.election_detail = function(req, res) {
 res.send('NOT IMPLEMENTED: Election detail: ' + req.params.id);
 };
-// Handle Costume create on POST.
+// Handle Election create on POST.
 exports.election_create_post = function(req, res) {
 res.send('NOT IMPLEMENTED: Election create POST');
 };
-// Handle Costume delete form on DELETE.
+// Handle Election delete form on DELETE.
 exports.election_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: Election delete DELETE ' + req.params.id);
 };
-// Handle Costume update form on PUT.
+// Handle Election update form on PUT.
 exports.election_update_put = function(req, res) {
 res.send('NOT IMPLEMENTED: Election update PUT' + req.params.id);
 };
 
-// List of all Costumes
+// List of all Elections
 exports.election_list = async function(req, res) {
     try{
     theElections = await Election.find();
