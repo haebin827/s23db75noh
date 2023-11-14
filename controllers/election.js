1f64c01dee8a0c801f6feef389d73e348bb0ec41
haebin827 <99354243+haebin827@users.noreply.github.com>
2023-11-13 18:20:22 -0600

    Part 5

--- controllers/election.js
+++ controllers/election.js
@@ -123,4 +123,18 @@ exports.election_view_one_Page = async function(req, res) {
   res.status(500)
   res.send(`{'error': '${err}'}`);
   }
-  };
+};
+
+  // Handle building the view for creating a costume.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.election_create_Page = function(req, res) {
+  console.log("create view")
+  try{
+  res.render('electioncreate', { title: 'Election Create'});
+  }
+  catch(err){
+  res.status(500)
+  res.send(`{'error': '${err}'}`);
+  }
+};
