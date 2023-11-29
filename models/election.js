58d9778a5dc0b1e337f61b3e2879190deacc68ab
haebin827 <99354243+haebin827@users.noreply.github.com>
2023-11-28 18:23:45 -0600

    UPDATE

--- models/election.js
+++ models/election.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 const electionSchema = mongoose.Schema({
     year: { type: Number, required: true, min: 1900, max: 3000 },
-    location: { type: String, required: true },
-    candidate: { type: String, required: true }
+    location: { type: String, required: true, min: 1, max: 100 },
+    candidate: { type: String, required: true, min: 1, max: 20 }
 });
 module.exports = mongoose.model("Election", electionSchema)
--- routes/elections.js
+++ routes/elections.js
@@ -15,8 +15,8 @@ router.get('/', election_controllers.election_view_all_Page);
 // router.get("/", election_controllers.election_list);
 // router.get('/:id', election_controllers.election_detail);
 router.get('/details', election_controllers.election_view_one_Page);
-router.get('/create', election_controllers.election_create_Page);
+router.get('/create', secured, election_controllers.election_create_Page);
 router.get('/update',secured, election_controllers.election_update_Page);
-router.get('/delete', election_controllers.election_delete_Page);
+router.get('/delete', secured, election_controllers.election_delete_Page);
 
 module.exports = router;
